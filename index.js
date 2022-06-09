a5ef51de7ba1fb5d681f5f5e5f688df6f8a2a2ee
allanstar-byte <allanmutisya263@gmail.com>
2022-06-09 05:03:25 +0300

    Arrays

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+
+var cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+
+destructivelyAppendCat('Ralph')
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+destructivelyPrependCat("Bob")
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+destructivelyRemoveLastCat()
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+destructivelyRemoveFirstCat()
+
+
+function appendCat(x) {
+    return [...cats, x]
+}
+appendCat("Broom")
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+prependCat("Arnold")
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1)
+}
+
+removeLastCat()
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
+
+removeFirstCat()
